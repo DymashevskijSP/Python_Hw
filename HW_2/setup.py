b2edfacceb642ac0be0dbe077e696b285fa3a02e
DymashevskijSP <sp.dymashevskij@gmail.com>
2022-02-16 23:13:14 +0300

    moved setup.py

--- HW_2/setup.py
+++ HW_2/setup.py
@@ -4,6 +4,6 @@ from os.path import join, dirname
 setup(
     name='megaserg01_astBuilder',
     version='1.6',
-    packages=["."],
+    packages=["HW_1"],
     long_description="Library to build ast for fibonacci function",
 )
